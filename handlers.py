91960be723bd7308ebfb88a0d2419ed3c602527a
PavelScherbatkin <Pahok1299@yandex.ru>
2021-04-30 10:57:16 +0300

    run_1

--- handlers.py
+++ handlers.py
@@ -14,7 +14,7 @@ client_key = trello_key
 client_secret = trello_secret
 
 from load_all import bot, dp, db
-from config import host
+host = '84.252.140.135'
 
 
 # Первый хендлер
@@ -130,7 +130,7 @@ async def oauth(message: types.Message):
     if check == False:
         request_token_url = 'https://trello.com/1/OAuthGetRequestToken'
         oauth = OAuth1Session(client_key, client_secret=client_secret)
-        oauth.redirect_uri = 'http://84.252.140.135:9090' # перенаправление на сервер
+        oauth.redirect_uri = f'http://{host}:9090' # перенаправление на сервер
         fetch_response = oauth.fetch_request_token(request_token_url)
         resource_owner_key = fetch_response.get('oauth_token')
         resource_owner_secret = fetch_response.get('oauth_token_secret')
@@ -145,20 +145,14 @@ async def oauth(message: types.Message):
 
         # Магия обработки url
         sock = socket.socket()
-        sock.bind(('84.252.140.135', 9090))
-        sock.listen(1)
-        conn, addr = sock.accept()
+        sock.connect((host, 9090))
 
-        print('Здесь происходить обработка перенаправленного url')
-        print('connected:', addr)
-        data = conn.recv(4096)
-        b = data.decode('utf-8').split(' ')[1]
-        url = f'http://84.252.140.135:9090' + b
-        conn.close()
-        print('connection close:')
+        data = sock.recv(1024)
+        print('Сервер прислал', data)
+        sock.close()
         # конец магии
-
-
+        await message.answer(data)
+        """
         # Парсинг url и получение токенов
         oauth.parse_authorization_response(url)
         access_token_url = 'https://trello.com/1/OAuthGetAccessToken'
@@ -175,6 +169,7 @@ async def oauth(message: types.Message):
             'Для получения справки введите /help'
             ]
         await message.answer('\n'.join(text))
+        """
     else:
         await message.answer('Вы уже авторизированы')
 
